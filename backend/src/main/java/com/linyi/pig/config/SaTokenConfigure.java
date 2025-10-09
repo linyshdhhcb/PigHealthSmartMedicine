6ace7e0c9d1d49ecc73ee172b484bed912e2486a
L382611 <14131958+hsdchb@user.noreply.gitee.com>
2025-10-09 22:08:50 +0800

    feat(auth): 增强Sa-Token拦截器配置请求跳过逻辑
    - 添加OPTIONS
    - 完善拦截器注释说明
    - 优化代码格式和结构

--- backend/src/main/java/com/linyi/pig/config/SaTokenConfigure.java
+++ backend/src/main/java/com/linyi/pig/config/SaTokenConfigure.java
@@ -9,11 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SaTokenConfigure implements WebMvcConfigurer {
-    // 注册拦截器
+
+    /**
+     * 添加拦截器配置
+     *
+     * @param registry 拦截器注册器，用于注册和配置拦截器
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        // 配置Sa-Token拦截器，用于权限验证和登录检查
         registry.addInterceptor(new SaInterceptor(handle -> {
                     System.out.println("--------- flag 2，请求进入了拦截器，访问的 path 是：" + SaHolder.getRequest().getRequestPath());
+                    // 跳过OPTIONS请求
+                    if ("OPTIONS".equals(SaHolder.getRequest().getMethod())) {
+                        return;
+                    }
                     StpUtil.checkLogin();  // 登录校验，只有会话登录后才能通过这句代码
                 }))
                 .addPathPatterns("/**")
@@ -34,7 +44,8 @@ public class SaTokenConfigure implements WebMvcConfigurer {
                         "/user/sendEmailCode",
                         "/user/logout",
                         "/user/checkLogin"
-                )
-        ;
+                );
     }
+
+
 }
